9bed0f794935c0eb9154c3b0a33f65a48b53e75a
bastien.tresse@gmail.com <bastien.tresse@gmail.com@e5b7d654-1cbe-daaf-6cf3-c3db0c7c8d4a>
2011-12-01 17:43:17 +0000

    passage à 2.3.3
    git-svn-id: https://utc-nuitinfo.googlecode.com/svn/trunk@6 e5b7d654-1cbe-daaf-6cf3-c3db0c7c8d4a

--- src/nuitinfo/appli/nuitinfoActivity.java
+++ src/nuitinfo/appli/nuitinfoActivity.java
@@ -1,43 +1,9 @@
 package nuitinfo.appli;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.jar.Attributes;
-
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.protocol.HTTP;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.DefaultHandler;
-
-import nuitinfo.bdd.*;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Looper;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.view.MotionEvent;
-import android.view.View;
 
 public class nuitinfoActivity extends Activity
 {
@@ -60,7 +26,7 @@ public class nuitinfoActivity extends Activity
 		// Récupère l'identifiant        
 		int user = i.getIntExtra("userid",-1);
 		 
-		//Affiche les identifiants de l'utilisateur
+		// Affiche les identifiants de l'utilisateur
         tv.setText("UserID: "+String.valueOf(user)+" logged in");
     }
  
